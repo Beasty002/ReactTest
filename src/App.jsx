e526e46576f81545d47a99d617e1a5065c6ae237
Beasty002 <xasbin12@gmail.com>
2025-01-16 21:33:51 +0545

    ft: option and variant selection done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import EcomVariant from './pages/ecomVariant'
 import SideBar from './components/sideBar'
+import Test from './pages/Test'
+
 
 const App = () => {
   return (
-    <>
+    <div className='flex'>
       <SideBar />
-      <main>
+      <main className='max-h-screen bg-[#f5f5f5] flex-1 p-4'>
         <Routes>
           <Route path='/' element={<EcomVariant />} />
+          <Route path='/test' element={<Test />} />
         </Routes>
       </main>
-    </>
+    </div>
   )
 }
 
